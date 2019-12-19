61ffe0d5ea191043725400f0ceb2ee261f6e5641
Anant Pawar <anant.c.pawar@gmail.com>
2019-12-19 21:49:08 +0530

    initial commit

---
+++ src/main/java/com/optile/cs/jmservice/JmServiceApplication.java
@@ -0,0 +1,13 @@
+package com.optile.cs.jmservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JmServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JmServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/optile/cs/jmservice/JmServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.optile.cs.jmservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JmServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
